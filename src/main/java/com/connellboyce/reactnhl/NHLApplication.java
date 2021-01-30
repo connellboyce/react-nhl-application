2fa26d17a2eb77c2f140ca7212b48ba9b2e94c55
Connell Boyce <connell.p.boyce@gmail.com>
2021-01-30 16:28:53 -0500

    Renamed main class

---
+++ src/main/java/com/connellboyce/reactnhl/NHLApplication.java
@@ -0,0 +1,14 @@
+package com.connellboyce.reactnhl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class NHLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NHLApplication.class, args);
+	}
+
+}
