7adc51e666efe6acf51c1f8d44e8fc642c84a5f4
Connell Boyce <connell.p.boyce@gmail.com>
2021-01-30 15:16:39 -0500

    Created Spring Data Team Repository
    - Spring Data path to Team Repository: teams
    - Allowed CrossOrigin requests

---
+++ src/main/java/com/connellboyce/reactnhl/repository/TeamRepository.java
@@ -0,0 +1,15 @@
+package com.connellboyce.reactnhl.repository;
+
+import com.connellboyce.reactnhl.model.Team;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "teams", path = "teams")
+public interface TeamRepository extends PagingAndSortingRepository {
+    List<Team> findByName(@Param("name") String name);
+}
