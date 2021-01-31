265841e84bcc5002301a12f8106873c35dad2cc2
Connell Boyce <connell.p.boyce@gmail.com>
2021-01-30 19:55:18 -0500

    Added display for NHL teams

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          error: null,
+          isLoaded: false,
+          teams: []
+      };
+  }
+
+  componentDidMount() {
+      fetch("http://localhost:9090/api/v1/teams")
+          .then(res => res.json())
+          .then(
+              (result) => {
+                  this.setState({
+                      isLoaded: true,
+                      teams: result._embedded.teams
+                  });
+              },
+              (error) => {
+                  this.setState({
+                      isLoaded: true,
+                      error
+                  });
+              }
+          )
+  }
+
+  render() {
+      const { error, isLoaded, teams } = this.state;
+      if (error) {
+          return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+          return <div>Loading...</div>
+      } else {
+          return (
+              <table>
+                  {teams.map(team => (
+                      <tr key={team.id}>
+                          <td><img alt={"Logo"} className={"teamLogo"} src={team.logoURL}/></td>
+                          <td>{team.location} {team.name}</td>
+                      </tr>
+                  ))}
+              </table>
+          );
+      }
+  }
 }
 
 export default App;
